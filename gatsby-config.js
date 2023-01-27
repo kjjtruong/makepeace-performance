067056ae71fc710ec8df7c95b1cacb5982b0f38b
Ken Truong <kjjtruong@gmail.com>
2023-01-27 10:10:53 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Makepeace Performance`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
