ea4ecf02acd11073875b630fe8576cb75152a0cf
Ken Truong <kjjtruong@gmail.com>
2023-01-30 14:38:49 -0700

    Blog Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `Makepeace Performance`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Seo from "../../components/seo";
+
+const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <GatsbyImage image={image} atl={data.mdx.frontmatter.hero_image_alt} />
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        slug
+        date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
+
+export default BlogPost;
